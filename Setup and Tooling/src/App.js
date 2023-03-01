801eac93c0be46add1a2ad2b0744129051b4155f
Joe Bosman <joseph.a.bosman@gmail.com>
2023-03-01 14:25:33 -0500

    Component reuse example

--- Setup and Tooling/src/App.js
+++ Setup and Tooling/src/App.js
@@ -1,9 +1,29 @@
-const App = () => { 
+function Pet({ name, animal, bread }){
   return (
     React.createElement(
       "div",
       {},
-      React.createElement("h1", {}, "Adopt Me!")
+      [
+        React.createElement("h1", {}, name  ),
+        React.createElement("h2", {}, animal  ),
+        React.createElement("h2", {}, bread )
+      ]
+    )
+  );
+}
+
+function App(){ 
+  return (
+    React.createElement(
+      "div",
+      {},
+      [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet,({name:"Luna",   animal: "Dog",  bread: "Havanese"})),
+        React.createElement(Pet,({name:"Pepper", animal: "Bird", bread: "Cockatiel"})),
+        React.createElement(Pet,({name:"Doink",  animal: "Cat",  bread: "Mixed"})),
+      ]
+      
     )
   );
 }
