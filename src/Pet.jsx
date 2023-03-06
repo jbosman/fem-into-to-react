82b8ef8bccaa22931e85598d421fc39e1b1fedb7
Joe Bosman <joseph.a.bosman@gmail.com>
2023-03-06 10:46:06 -0500

    Refactored Pet with JSX

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,14 +1,26 @@
-function Pet({ name, animal, bread }) {
+// First iteration without using JSX
+// function Pet({ name, animal, bread }) {
+//   return (
+//     React.createElement(
+//       "div",
+//       {},
+//       [
+//         React.createElement("h1", {}, name),
+//         React.createElement("h2", {}, animal),
+//         React.createElement("h2", {}, bread)
+//       ]
+//     )
+//   );
+// }
+
+// JSX version
+function Pet(props){
   return (
-    React.createElement(
-      "div",
-      {},
-      [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, bread)
-      ]
-    )
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.bread}</h2>
+    </div>
   );
 }
 
