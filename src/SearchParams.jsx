97947b43ced21b397bad2b8d1e3be8b864b35245
Joe Bosman <joseph.a.bosman@gmail.com>
2023-03-06 12:28:05 -0500

    Setup a useEffect hook for fetching data

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,16 +1,39 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import Pet from './Pet';
 
 const ANIMALS = [ "bird", "cat", "dog", "rabbit", "reptile" ];
-const BREEDS = [];
 
 function SearchParams(){
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const breeds = [];
+
+  
+  useEffect(() => { 
+    requestPets(); 
+  }, []);// eslint-disable-line react-hooks/exhaustive-deps
+
+  async function requestPets(){
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
-      <form>
+      <form 
+        onSubmit={ e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -39,15 +62,25 @@ function SearchParams(){
           Breed
           <select
             onChange={(e) => setBreed(e.target.value)}
-            disabled={BREEDS.length === 0} 
+            disabled={breeds.length === 0} 
             id="breed"
             value={breed} 
           >
             <option/>
-            { BREEDS.map( breed => <option key={breed}>{breed}</option> ) }
+            { breeds.map( breed => <option key={breed}>{breed}</option> ) }
           </select>
         </label>
         <button>Submit</button>
+        {
+          pets.map( (pet) => (
+            <Pet
+              key={pet.id}
+              name={pet.name} 
+              animal={pet.animal} 
+              breed={pet.breed} 
+            /> 
+          ))
+        }
       </form>
     </div>
   );
