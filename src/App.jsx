9b6cdfa666d6125f8a2f1f2bfbb7e1c60c97069a
Joe Bosman <joseph.a.bosman@gmail.com>
2023-03-21 11:37:46 -0400

    Removed non-jsx component components

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,6 @@ import { createRoot } from 'react-dom/client';
 
 import SearchParams from './SearchParams';
 
-// Original App component without JSX
-// function App() {
-//   return (
-//     React.createElement(
-//       "div", 
-//       {},
-//       [
-//         React.createElement("h1", {}, "Adopt Me!"),
-//         React.createElement(Pet, ({ name: "Luna", animal: "Dog", bread: "Havanese" })),
-//         React.createElement(Pet, ({ name: "Pepper", animal: "Bird", bread: "Cockatiel" })),
-//         React.createElement(Pet, ({ name: "Doink", animal: "Cat", bread: "Mixed" })),
-//       ]
-//     )
-//   );
-// }
-
 // App with JSX
 function App(){
   return (
--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,18 +1,3 @@
-// First iteration without using JSX
-// function Pet({ name, animal, bread }) {
-//   return (
-//     React.createElement(
-//       "div",
-//       {},
-//       [
-//         React.createElement("h1", {}, name),
-//         React.createElement("h2", {}, animal),
-//         React.createElement("h2", {}, bread)
-//       ]
-//     )
-//   );
-// }
-
 // JSX version
 function Pet(props){
   return (
